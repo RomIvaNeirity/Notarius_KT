e461383c9e5d5cb2614009b3d141d0b0f42824a1
Roman <neirity@gmail.com>
2025-07-12 14:28:19 +0300

    fix scroll

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -15,7 +15,7 @@
       // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
       refs.modal.classList.toggle("is-open");
     }
-})(); */
+})(); 
   
 
 (() => {
@@ -38,4 +38,33 @@
     refs.modal.classList.remove("is-open");
     document.body.classList.remove("menu-open");
   }
-})();
+})();*/
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-mob-open]"),
+    closeModalBtn: document.querySelector("[data-mob-close]"),
+    modal: document.querySelector("[data-mob]"),
+  };
+
+  if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) {
+    console.warn('One or more modal elements not found in DOM');
+    return;
+  }
+
+  refs.openModalBtn.addEventListener("click", openMenu);
+  refs.closeModalBtn.addEventListener("click", closeMenu);
+
+  function openMenu() {
+    refs.modal.classList.add("is-open");
+    document.body.classList.add("menu-open");
+    document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
+  }
+
+  function closeMenu() {
+    refs.modal.classList.remove("is-open");
+    document.body.classList.remove("menu-open");
+    document.body.style.paddingRight = "";
+  }
+});
